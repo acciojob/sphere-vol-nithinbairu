4bd59f33e7eb9ee483ef745d48b3e8a025b60530
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-29 11:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
     //Write your code here
-  
+  document.getElementById("submit").addEventListener("click",()=>{
+	  const radius=document.getElementById("radius").value;
+	  const volume=(4 / 3) * Math.PI * Math.pow(radius, 3);
+	  const app=Math.floor(volume*10000)/10000
+	  const vol=document.getElementById("volume");
+	  // console.log(app)
+	  vol.value=`Calculated volume should be approximately ${app}`;
+	  console.log(vol.value)
+  })
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
