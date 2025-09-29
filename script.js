2eade3b28c4b92c1c5295d9c34de2d212292cee3
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-29 12:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function volume_sphere() {
-    //Write your code here
-  document.getElementById("submit").addEventListener("click",()=>{
-	  const radius=document.getElementById("radius").value;
-	  const volume=(4 / 3) * Math.PI * Math.pow(radius, 3);
-	  const app=Math.floor(volume*10000)/10000
-	  const vol=document.getElementById("volume");
-	  // console.log(app)
-	  vol.value=`Calculated volume should be approximately ${app}`;
-	  console.log(vol.value)
-  })
+  document.getElementById("submit").addEventListener("click", () => {
+    const radiusInput = document.getElementById("radius").value;
+    const vol = document.getElementById("volume");
+
+    const radius = parseFloat(radiusInput);
+
+    if (!isNaN(radius) && radius > 0) {
+      const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+      const roundedVolume = Math.floor(volume * 10000) / 10000;
+      vol.value = `Calculated volume should be approximately ${roundedVolume}.`;
+    } else {
+      vol.value = `Calculated volume should be 'NaN' due to invalid input.`;
+    }
+  });
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
